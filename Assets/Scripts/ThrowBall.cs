278cc8d4410c034a6054d5262159fcdfa8ea2e87
KolokolovaVik <2645436@gmail.com>
2024-12-19 11:42:51 +0300

    Add bool _isBallInAir
    protection against multiple button presses while the ball is in the air

--- Assets/Scripts/ThrowBall.cs
+++ Assets/Scripts/ThrowBall.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Rigidbody))] 
+[RequireComponent(typeof(Rigidbody))]
 public class ThrowBall : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbbody;
     [SerializeField] private Transform _pointBall;
     [SerializeField] private Transform _target;
     [SerializeField] private Button _throwButton;
+
+    private bool _isBallInAir = false;
 
     private void Awake()
     {
@@ -16,12 +18,21 @@ public class ThrowBall : MonoBehaviour
 
     private void Start()
     {
-        _throwButton.onClick.AddListener(Throw);
+        _throwButton.onClick.AddListener(OnThrowButtonClick);
+    }
+
+    private void OnThrowButtonClick()
+    {
+        if (!_isBallInAir)
+        {
+            Throw();
+        }
     }
 
     private void Throw()
     {
         _rigidbbody.isKinematic = false;
+        _isBallInAir = true;
         Vector3 directionThrow = _target.position - _pointBall.position;
         _rigidbbody.AddForce(directionThrow, ForceMode.Impulse);
     }
